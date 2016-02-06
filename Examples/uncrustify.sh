7c516958302a834471212d78dbcf6a6dff22f9b7
Ignacio Romero Zurbuchen <iromero@dzen.cl>
2016-02-06 12:34:05 -0300

    Updates Uncrustify bash script

--- Examples/uncrustify.sh
+++ Examples/uncrustify.sh
@@ -1,4 +1,14 @@
 # uncrustify 0.61
 # https://github.com/bengardner/uncrustify
 
-${SRCROOT}/uncrustify -l OC -q -c ${SRCROOT}/uncrustify_config.cfg ${SRCROOT}/**/*.[ch,cm] ../Source/*.[ch,cm] --no-backup stop > /dev/null 2>&1 || true
+# paths variables
+EXEC_PATH=${SRCROOT}/uncrustify
+CONFIG_PATH=${SRCROOT}/uncrustify_config.cfg
+
+# executes uncrustfy
+# sets language to Objective-C
+# sets Quiet mode (no output)
+# uses the config file CFG
+# overrides all files
+# fails silentely
+${EXEC_PATH} -l OC -q -c ${CONFIG_PATH} ${SRCROOT}/**/*.[ch,cm] ../Source/*.[ch,cm] --no-backup stop > /dev/null 2>&1 || true
